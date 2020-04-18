250f0504cb831a06f6ef0f39ee8810c5f25da97f
dimitrov-k <63186381+dimitrov-k@users.noreply.github.com>
2020-04-18 21:54:04 +0300

    new include added

--- source/user_interface.h
+++ source/user_interface.h
@@ -1,17 +1,18 @@
 #pragma once
 #include "chess.h"
+#include "game.h"
 
 #define WHITE_SQUARE 0xDB
 #define BLACK_SQUARE 0xFF
 #define EMPTY_SQUARE 0x20
 
-void createNextMessage( string msg );
-void appendToNextMessage( string msg );
-void clearScreen( void );
-void printLogo( void );
-void printLogo( void );
-void printMenu( void );
-void printMessage( void );
-void printLine( int line, int color1, int color2, Game& game );
-void printSituation( Game& game );
+void createNextMessage(string msg);
+void appendToNextMessage(string msg);
+void clearScreen(void);
+void printLogo(void);
+void printLogo(void);
+void printMenu(void);
+void printMessage(void);
+void printLine(int line, int color1, int color2, Game& game);
+void printSituation(Game& game);
 void printBoard(Game& game);
